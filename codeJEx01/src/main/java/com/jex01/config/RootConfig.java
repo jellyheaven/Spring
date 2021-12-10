a41a698ecbb4161ac728163cb72b1b1b8286fea1
jellyheaven <mayatoto@naver.com>
2021-12-10 16:43:13 +0900

    스프링 MVC 설정 Java

---
+++ codeJEx01/src/main/java/com/jex01/config/RootConfig.java
@@ -0,0 +1,8 @@
+package com.jex01.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+
+}
---
+++ codeJEx01/src/main/java/com/jex01/domain/SampleDTOList.java
@@ -0,0 +1,12 @@
+package com.jex01.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SampleDTOList {
+	private List<SampleDTO> list;
+	
+	public SampleDTOList() {
+		list = new ArrayList<SampleDTO>();
+	}
+}
